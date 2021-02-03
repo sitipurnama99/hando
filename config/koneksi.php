822a904ff334390376498e3b0fef4e05a4dbc2b2
sitipurnama99 <sitipurnama378@gmail.com>
2021-02-03 19:06:51 +0700

    update config database

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $db_hostname = 'localhost';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'hando';
+$db_username = 'id16074865_handosttb';
+$db_password = '|s1Q7BFK+oa(2q<H';
+$db_name = 'id16074865_hando';
 try {
     $koneksi = new PDO(
         "mysql:host=$db_hostname;dbname=$db_name",
